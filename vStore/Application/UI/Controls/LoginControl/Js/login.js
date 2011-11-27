7bf4a98c02e91c0d1da5c0a2392d15a3541b8105
Jirka Vebr <jirkavebr@gmail.com>
2011-11-27 12:32:16 +0100

    Simple forum forced login

--- vStore/Application/UI/Controls/LoginControl/Js/login.js
+++ vStore/Application/UI/Controls/LoginControl/Js/login.js
@@ -2,5 +2,14 @@ $(function () {
 
 	$('a.retrievePasswordLink').colorbox();
 
-	$('#loginForm').find('form').colorboxForm();
+	$('#loginForm').find('form').colorboxForm({
+		onSuccess: function () {
+			var doc = $(document);
+			if($.data(doc.get(0), 'events').cbox_loggedIn) {
+				doc.trigger('cbox_loggedIn');
+			} else {
+				window.location.reload(false);
+			}
+		}
+	});
 });
